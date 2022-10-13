d5c5aaaf3d971deecee65bcdf7fa60408e922578
Kunal jain <72144717+kunaljainwin@users.noreply.github.com>
2022-10-13 21:49:14 +0530

    Time: 23 ms (22.92%), Space: 14 MB (56.24%) - LeetHub

---
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int x=0;
+        for(auto &it:operations){
+            if(it[0]=='+'||it[2]=='+'){
+                x++;
+                
+            }
+            else{
+                x--;
+            }
+        }
+        return x;
+    }
+};
