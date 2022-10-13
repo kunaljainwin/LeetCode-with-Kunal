82c92dcc7b0c8c693d10e45ff6cd03bca19e1657
Kunal jain <72144717+kunaljainwin@users.noreply.github.com>
2022-10-14 00:14:06 +0530

    Time: 10 ms (14.73%), Space: 6.2 MB (94.39%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -5,8 +5,8 @@ public:
         
         for(int i=0;i<n-1;i++){
             if(abs(s[i]-s[i+1])==97-65){
-                s.erase(s.begin()+i);
-                s.erase(s.begin()+i);
+                s.erase(s.begin()+i,s.begin()+i+2);
+          
                 // cout<<s<<endl;
                 n-=2;
                 i=-1;
