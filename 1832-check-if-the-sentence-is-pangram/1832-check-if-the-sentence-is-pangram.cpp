9b350980da38c39704f3e78ca843c66b0a46d7c3
Kunal jain <72144717+kunaljainwin@users.noreply.github.com>
2022-10-17 06:06:40 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (94.60%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     bool checkIfPangram(string s) {
+	   // Just make an character count array and check if count of each character is  atleast .
 
         vector<int>arr(26,0);
         for(auto &it :s)arr[it-'a']++;
         
         for(int i=0;i<26;i++)if(arr[i]<1)return false;
-        
-        
         return true;
     }
 };
