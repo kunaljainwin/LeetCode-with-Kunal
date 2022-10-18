4c0792ba335b63e08272e76f8be5a38b9b379e4f
Kunal jain <72144717+kunaljainwin@users.noreply.github.com>
2022-10-18 06:38:33 +0530

    Time: 13 ms (61.78%), Space: 6.6 MB (77.40%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        // map can be userd
+        if(n==1)
+            return "1";
+        string say=countAndSay(n-1);
+    int count=0;
+        char c=say[0];
+               string result="";
+        for(int i=0;i<say.length();i++){
+            if(say[i]==c){
+                count++;
+            }else{
+                result+=to_string(count)+c;
+                count=0;
+                c=say[i];
+                i--;
+              
+            }
+        }
+        if(count>0){
+              result+=to_string(count)+c;
+        }
+ 
+        return result;
+    }
+};
