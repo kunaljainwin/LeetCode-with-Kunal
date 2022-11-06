5c159834eb9d55dbb5f6d60eaed265e49fa1eae3
Kunal jain <72144717+kunaljainwin@users.noreply.github.com>
2022-11-07 01:21:46 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (75.43%) - LeetHub

--- 0899-orderly-queue/0899-orderly-queue.cpp
+++ 0899-orderly-queue/0899-orderly-queue.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     string orderlyQueue(string s, int k) {
+        int n=s.length();
         string ans=s;
+        
         if(k>1){
             sort(s.begin(),s.end());
             ans=s;
         }
         else{
-        int n=s.length();
-       
-
         while(1+n--){
             string next=s;
             next.erase(next.begin());
