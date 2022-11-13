df15676b79314b27514c9980996dd724f6a1592d
Kunal jain <72144717+kunaljainwin@users.noreply.github.com>
2022-11-13 07:30:18 +0530

    Time: 12 ms (56.07%), Space: 7 MB (91.09%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,12 +1,49 @@
 class Solution {
 public:
     string reverseWords(string s) {
-      stringstream is(s);
-      string str,ans="";
-      while(is>>str){
-        ans=str+" "+ans;
-      }
-      return ans.substr(0,ans.end()-ans.begin()-1);
-      
+        /* In place reversal */
+        char c;
+        int n = s.size();
+        for (int k = 0; k < s.size()/2; k++) {
+            swap(s[k] ,s[n - 1 - k]);
+        }
+        
+        /* Identify all words */
+        int ini = -1;
+        int fin = -1;
+        for (int k = 0; k <= s.size(); k++) {
+            if (k == s.size() || s[k] == ' ') { // Need to accommodate end of string ($)
+                fin = k;
+                if (ini != -1) {
+                    n = fin - ini;
+                    for (int k = 0; k < n/2; k++) {
+                        c = s[ini + k];
+                        s[ini + k] = s[ini + n - 1 - k];
+                        s[ini + n - 1 - k] = c;
+                    }
+                }
+                ini = -1;
+            }
+            else if (ini == -1) {
+                ini = k;
+            }
+        }
+        
+        /* Trim all spaces */
+        ini = 0;
+        n = 1;
+        for (fin = 0; fin < s.size(); fin++) {
+            if (s[fin] == ' ' && n == 0) {
+                s[ini] = s[fin];
+                ini++;
+                n++;
+            }
+            if (s[fin] != ' ') {
+                s[ini] = s[fin];
+                ini++;
+                n = 0;
+            }
+        }
+        return s.substr(0, s[ini - 1] == ' ' ? ini - 1 : ini); 
     }
 };
