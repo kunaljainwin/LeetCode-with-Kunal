b15f35e19e3b7f55632b54f2108e89e02fe84915
Kunal jain <72144717+kunaljainwin@users.noreply.github.com>
2022-11-16 00:31:22 +0530

    Time: 1 ms (27.42%), Space: 50.2 MB (27.73%) - LeetHub

---
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -0,0 +1,11 @@
+class Solution {
+   int count=0;
+    public int countNodes(TreeNode root) {
+       if(root==null) return 0;
+        count++;
+        countNodes(root.left);
+        countNodes(root.right);
+        
+        return count;
+    }
+}
