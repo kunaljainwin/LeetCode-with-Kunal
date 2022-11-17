37067f3369dc30d985f2c192e34c7d350a0cf1e1
Kunal jain <72144717+kunaljainwin@users.noreply.github.com>
2022-11-17 09:44:05 +0530

    Time: 24 ms (18.00%), Space: 5.9 MB (75.43%) - LeetHub

--- 0223-rectangle-area/0223-rectangle-area.cpp
+++ 0223-rectangle-area/0223-rectangle-area.cpp
@@ -8,14 +8,15 @@ public:
         // sum areas and subtract common
         // coordinate of common will be ? will common wil be there
         // find intersecting or not
-        if (ax2 <= bx1 || ay1 >= by2 || ax1 >= bx2 || ay2<= by1) {
-            return area;
-        }
+        if (ax2 <= bx1 || ay1 >= by2 || ax1 >= bx2 || ay2<= by1) {}
         //if it has overlap
-    int a1 =min(ax2, bx2) - max(ax1, bx1);
+        else{
+     int a1 =min(ax2, bx2) - max(ax1, bx1);
     int a2 =min(ay2, by2) - max(ay1, by1);
     int overlap = a1 * a2;
     area-=overlap;
+        }
+   
         return area;
     }
 };
