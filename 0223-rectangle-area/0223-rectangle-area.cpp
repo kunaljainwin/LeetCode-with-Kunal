4da1ff3001b8038300016947d83794a019ddf264
Kunal jain <72144717+kunaljainwin@users.noreply.github.com>
2022-11-17 09:43:02 +0530

    Time: 25 ms (14.84%), Space: 5.9 MB (75.43%) - LeetHub

---
+++ 0223-rectangle-area/0223-rectangle-area.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int areaOfRect(int x1,int y1,int x2 ,int y2){
+        return abs((y2-y1)*(x2-x1));
+    }
+    int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+        int area= areaOfRect(ax1,ay1,ax2,ay2)+areaOfRect(bx1,by1,bx2,by2);
+        // sum areas and subtract common
+        // coordinate of common will be ? will common wil be there
+        // find intersecting or not
+        if (ax2 <= bx1 || ay1 >= by2 || ax1 >= bx2 || ay2<= by1) {
+            return area;
+        }
+        //if it has overlap
+    int a1 =min(ax2, bx2) - max(ax1, bx1);
+    int a2 =min(ay2, by2) - max(ay1, by1);
+    int overlap = a1 * a2;
+    area-=overlap;
+        return area;
+    }
+};
