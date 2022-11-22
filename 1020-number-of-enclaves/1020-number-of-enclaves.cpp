a9730129e19fab33e94ebf6fc921a121ec6dd045
Kunal jain <72144717+kunaljainwin@users.noreply.github.com>
2022-11-22 22:42:38 +0530

    Time: 211 ms (10.79%), Space: 29.2 MB (67.65%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    //return -1 if not otherwise size
+    int isEnclaved(vector<vector<int>>&grid,int i,int j,int &m,int &n){
+        
+        if(i<0||i>=m||j<0||j>=n){
+            return INT_MIN;
+        }
+        if(grid[i][j]==0){
+            return 0;
+        }
+        grid[i][j]=0;
+        int a=isEnclaved(grid,i,j+1,m,n);
+        int b=isEnclaved(grid,i+1,j,m,n);
+        int c=isEnclaved(grid,i-1,j,m,n);
+        int d=isEnclaved(grid,i,j-1,m,n);
+        if( a<0||b<0||c<0||d<0)
+            return INT_MIN;
+        
+        return 1+ a+b+c+d;
+        
+    }
+    int numEnclaves(vector<vector<int>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+        // 1 represents land
+        // a move 4 directions
+        int ans=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1){
+                    int a=isEnclaved(grid,i,j,m,n);
+                    if(a>-1){
+                     ans+=a;   
+                    }
+                        
+                }
+            }
+        }
+        return ans;
+    }
+};
