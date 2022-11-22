6323ba1db52034b81706ae94f5e7cc6495c8a4bc
Kunal jain <72144717+kunaljainwin@users.noreply.github.com>
2022-11-22 22:14:01 +0530

    Time: 35 ms (21.29%), Space: 14.3 MB (5.05%) - LeetHub

--- 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.cpp
+++ 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.cpp
@@ -1,22 +1,69 @@
 class Solution {
 public:
     int findTheDistanceValue(vector<int>& arr1, vector<int>& arr2, int d) {
-        // brute force
-        int count=0;
+        // binary seach on 2nd array
         int n1=arr1.size();
-        int n2=arr2.size();
+        set<int>s;
+        int count=0;
+        for(auto &it:arr2)
+            s.insert(it);
         for(int i=0;i<n1;i++){
-            bool flag=true;
-            for(int j=0;j<n2;j++){
-                if(abs(arr1[i]-arr2[j])<=d){
-                    flag=false;
-                    break;
+            // find closest element to i th in arr1
+            auto x=s.lower_bound(arr1[i]);
+            if(x==s.begin()){
+                if(abs(arr1[i]-*x)<=d){
+                    count++;
                 }
             }
-            if(flag){
-                count++;
+                else if(x==s.end()){
+                    if(abs(arr1[i]-*s.rbegin())<=d){
+                    count++;
+                }
+                }
+            
+                else{
+                     if(abs(arr1[i]-*x)<=d){
+                    count++;
+                         continue;
+                }
+                    x--;
+                    
+                    if(arr1[i]-  *(x)<=d){
+                      
+                        count++;
+                    }
+                }
             }
-        }
-        return count;
+        
+                    return n1-count;
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // brute force
+        
+        // int count=0;
+        // int n1=arr1.size();
+        // int n2=arr2.size();
+        // for(int i=0;i<n1;i++){
+        //     bool flag=true;
+        //     for(int j=0;j<n2;j++){
+        //         if(abs(arr1[i]-arr2[j])<=d){
+        //             flag=false;
+        //             break;
+        //         }
+        //     }
+        //     if(flag){
+        //         count++;
+        //     }
+        // }
+        // return count;
     }
 };
