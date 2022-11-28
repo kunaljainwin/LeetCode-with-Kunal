c8c93afdf837a0704c09aee4c1805da1313eac1e
Kunal jain <72144717+kunaljainwin@users.noreply.github.com>
2022-11-28 17:48:51 +0530

    Time: 15 ms (27.84%), Space: 10 MB (5.71%) - LeetHub

---
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isAlienSorted(vector<string>& words, string order) {
+        unordered_map<char,char>mp;
+        char c='a';
+        for(auto &it:order){
+            mp[it]=c;
+            c++;
+        }
+          for(auto &it:order){
+cout<<it<<" ";
+        }
+        for(auto &it1:words){
+            for(auto &it2:it1){
+                it2=mp[it2];
+            }
+        }
+        vector<string> w(words.begin(),words.end());
+        sort(w.begin(),w.end());
+        for(int i=0;i<words.size();i++){
+            if(words[i].compare(w[i])!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
